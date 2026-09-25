37cdb364ca339de081700d24224ead7f61621576
Cleberamonjr <cleberamjr@gmail.com>
2026-09-25 13:34:52 +0800

    fix: force installed PWA to pick up latest build

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const CACHE = 'gde-v4';
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
+const CACHE = 'gde-v5';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})).then(() => self.skipWaiting()));
